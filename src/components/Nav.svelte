1693791b569c1ac79ed1f18b26cae648aabc9b6e
Shaws <seanrichardparker@gmail.com>
2020-08-12 09:11:30 -0400

    sticky nav

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -7,6 +7,10 @@
     border-bottom: 1px solid rgba(255, 62, 0, 0.1);
     font-weight: 300;
     padding: 0 1em;
+    position: sticky;
+    top: 0;
+    z-index: 2;
+    background: white;
   }
 
   ul {
